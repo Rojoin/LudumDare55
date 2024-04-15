674ad2355358a72204dcf89b8c20474414575cb8
Nicodragon25 <nicolasariellaure@gmail.com>
2024-04-15 17:57:05 -0300

    Add mobile check and empty buttons

---
+++ Assets/Scripts/Mobile/MobileButtons.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileButtons : MonoBehaviour
+{
+    [SerializeField] GameObject leftButton;
+    [SerializeField] GameObject rightButton;
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+    [System.Runtime.InteropServices.DllImport("__Internal")]
+    private static extern bool IsMobile();
+#endif
+
+    private void Awake()
+    {
+        if (!CheckIfMobile())
+        {
+            Destroy(leftButton);
+            Destroy(rightButton);
+        }
+    }
+
+    private bool CheckIfMobile()
+    {
+        bool isMobile = false;
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+        isMobile = IsMobile();
+#endif
+
+        return isMobile;
+    }
+}
